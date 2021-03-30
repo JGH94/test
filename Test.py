aaefad6ef1fbad5f3d8a9d4c319e560285f63118
jgh910000 <jgh910000@naver.com>
2021-03-30 13:15:13 +0900

    Initial commit

---
+++ Test.py
@@ -0,0 +1,19 @@
+import New as new
+import TT as tt
+class MMain:
+    def GuGudan(num):
+        print(str(num) + "단 시작!!**********************************************")
+        for z in range(1, 10):
+            z_ = z;
+            re_ = num * z_
+            print((str(num) + "*" + str(z_) + "=" + str(re_)))
+
+
+def main():
+    MMain.GuGudan(5)
+    re_ = new.Add(5,5)
+    re2_ = new.min(5,2)
+    re3_ = tt.Test("정근화")
+    print(re3_)
+if __name__ == "__main__":
+    main()
